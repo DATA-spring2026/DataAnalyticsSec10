81f5e887f874756868a7e4851a8328ff7fc1c0d1
elizabeas <bess@rcc.team>
2026-05-04 15:44:49 -0500

    Added first Python script example

---
+++ Week-05/example.py
@@ -0,0 +1,27 @@
+# Sample script - getting started with Python
+
+print("Hello world!")
+
+
+# Creating some variables
+a = "hello"
+b = "world!"
+c = "Section"
+d = 10
+
+# Variations on print statements
+print("First a/b output: ", a, b)
+
+print("Second a/b output: " + a + " " + b)  # example of plus sign as string concatentation
+
+print("Third a/b output: ", a, "\n", b)  # use of newline string to create line break
+
+print("Fourth a/b output: ")
+print(a)
+print(b)
+
+print()
+
+print(a, b, c, d)
+
+print(a + " " + b + " " + c + ' ' + str(d))
